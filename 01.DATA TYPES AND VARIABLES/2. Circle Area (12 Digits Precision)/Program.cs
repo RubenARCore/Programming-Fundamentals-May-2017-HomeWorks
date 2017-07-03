67720cf8c421f41c4d75215b62faa410f0b4b5a8
RubenYordanov <yordanov.ruben@gmail.com>
2017-07-03 22:00:39 +0300

    up folders

---
+++ 01.DATA TYPES AND VARIABLES/2. Circle Area (12 Digits Precision)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Circle_Area__12_Digits_Precision_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+            double radius = Math.PI * r * r;
+            Console.WriteLine($"{radius:f12}");
+        }
+    }
+}
---
+++ 01.DATA TYPES AND VARIABLES/6. Triples of Letters/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Triples_of_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    for (int k = 0; k < number; k++)
+                    {
+                        char letter1 = (char) ('a' + i);    
+                        char letter2 = (char) ('a' + j);
+                        char letter3 = (char) ('a' + k);
+                        Console.WriteLine(""+letter1+letter2+letter3);
+                    }
+                }
+            }
+        }
+    }
+}
